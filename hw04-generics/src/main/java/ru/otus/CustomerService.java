beded87e16fe28a01bddb245c1ce9c6235967325
ruslan.kadyrov <ruslan.kadyrov@sravni.ru>
2024-02-13 15:39:45 +0300

    Homework 2

--- hw04-generics/src/main/java/ru/otus/CustomerService.java
+++ hw04-generics/src/main/java/ru/otus/CustomerService.java
@@ -1,22 +1,38 @@
 package ru.otus;
 
 import java.util.Map;
+import java.util.TreeMap;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
 
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> sortedCustomers = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> firstEntry = sortedCustomers.firstEntry();
+        if (firstEntry != null) {
+            return Map.entry(
+                    new Customer(firstEntry.getKey().getId(), firstEntry.getKey().getName(), firstEntry.getKey().getScores()),
+                    firstEntry.getValue()
+            );
+        } else {
+            return null;
+        }
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> higherEntry = sortedCustomers.higherEntry(customer);
+        if (higherEntry != null) {
+            return Map.entry(
+                    new Customer(higherEntry.getKey().getId(), higherEntry.getKey().getName(), higherEntry.getKey().getScores()),
+                    higherEntry.getValue()
+            );
+        } else {
+            return null;
+        }
     }
 
     public void add(Customer customer, String data) {
+        sortedCustomers.put(customer, data);
     }
+
 }
