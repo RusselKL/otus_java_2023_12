47f204feacfe91bac018129252630be42b39349e
ruslan.kadyrov <ruslan.kadyrov@sravni.ru>
2024-05-20 17:59:33 +0300

    Homework 22 init

---
+++ hw22-cache/homework-template/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,32 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "phone")
+public class Phone {
+    @Id
+    @SequenceGenerator(name = "phone_gen", sequenceName = "phone_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_gen")
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Column(name = "number", unique = true)
+    private String number;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+}
