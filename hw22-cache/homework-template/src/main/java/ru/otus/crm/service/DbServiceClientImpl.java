d968dd7d7dacadef89b44f221c728b22950e8f48
ruslan.kadyrov <ruslan.kadyrov@sravni.ru>
2024-05-27 19:03:49 +0300

    Homework 22

--- hw22-cache/homework-template/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw22-cache/homework-template/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -1,22 +1,30 @@
 package ru.otus.crm.service;
 
-import java.util.List;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionManager;
 import ru.otus.crm.model.Client;
+
+import java.util.List;
+import java.util.Optional;
 
 public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
 
     private final DataTemplate<Client> clientDataTemplate;
     private final TransactionManager transactionManager;
+    private final HwCache<String, Client> cache;
 
-    public DbServiceClientImpl(TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate) {
+    public DbServiceClientImpl(
+            TransactionManager transactionManager,
+            DataTemplate<Client> clientDataTemplate,
+            HwCache<String, Client> cache
+    ) {
         this.transactionManager = transactionManager;
         this.clientDataTemplate = clientDataTemplate;
+        this.cache = cache;
     }
 
     @Override
@@ -26,16 +34,22 @@ public class DbServiceClientImpl implements DBServiceClient {
             if (client.getId() == null) {
                 var savedClient = clientDataTemplate.insert(session, clientCloned);
                 log.info("created client: {}", clientCloned);
+                cache.put(String.valueOf(savedClient.getId()), savedClient);
                 return savedClient;
             }
             var savedClient = clientDataTemplate.update(session, clientCloned);
             log.info("updated client: {}", savedClient);
+            cache.put(String.valueOf(savedClient.getId()), savedClient);
             return savedClient;
         });
     }
 
     @Override
     public Optional<Client> getClient(long id) {
+        var client = Optional.ofNullable(cache.get(String.valueOf(id)));
+        if (client.isPresent()) {
+            return client;
+        }
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientOptional = clientDataTemplate.findById(session, id);
             log.info("client: {}", clientOptional);
